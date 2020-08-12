e996f94f5ec52b80371bfb4585f629746b06c80a
Jagrit Thapar <jagritthapar007@gmail.com>
2020-08-12 19:12:37 +0530

    Update: 1.0.8 - Heroku Read Environment - Forgot to add this in the first place. - Still didn't test with heroku deploy button - Try it and if there's any bug left, open an ISSUE.

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,7 @@
+import os
+from distutils.util import strtobool as stb
+
+# --------------------------------------
 BOT_TOKEN = ""
 GDRIVE_FOLDER_ID = ""
 OWNER_ID = 123455673
@@ -5,3 +9,15 @@ OWNER_ID = 123455673
 INDEX_URL = ""
 IS_TEAM_DRIVE = True
 USE_SERVICE_ACCOUNTS = True
+# --------------------------------------
+
+# dont edit below this >
+
+
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN', BOT_TOKEN)
+GDRIVE_FOLDER_ID = os.environ.get('GDRIVE_FOLDER_ID', GDRIVE_FOLDER_ID)
+OWNER_ID = int(os.environ.get('OWNER_ID', OWNER_ID))
+INDEX_URL = os.environ.get('INDEX_URL', INDEX_URL)
+IS_TEAM_DRIVE = stb(os.environ.get('IS_TEAM_DRIVE', IS_TEAM_DRIVE))
+USE_SERVICE_ACCOUNTS = stb(os.environ.get('USE_SERVICE_ACCOUNTS', USE_SERVICE_ACCOUNTS))
